0e2a065fe886251ade34126af4ee2ef71ec917a8
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2025-06-17 11:56:10 +0545

    lab 03 ver 6

--- Lab 03/signum.m
+++ Lab 03/signum.m
@@ -1,17 +1,19 @@
-
 x = zeros(size(t)); 
 
 t = -2:0.1:2;
+
 for i = 1:length(t)
-    if t(i)>=0
-        x(i) = t(i);
-    else
+    if t(i) > 0
+        x(i) = 1;
+    elseif t(i) == 0
         x(i) = 0;
+    else
+        x(i) = -1;
     end
 end
 subplot(2,1,1)
 plot (t,x, '-red')
-legend("Ramp function")
+legend("Signum function")
 title('Lab3/Aviskar Poudel/Ramp - Continuous')
 
 subplot(2,1,2)
