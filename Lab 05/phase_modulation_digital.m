5f7248c991cd9c8d25f2c632dba15b01bf2f72ac
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2025-07-22 09:44:12 +0545

    lab5 final

---
+++ Lab 05/phase_modulation_digital.m
@@ -0,0 +1,39 @@
+clc;
+clear all;
+close all;
+
+Amp = 5;
+fm = 2;   % Message frequency (binary)
+fc = 10;  % Carrier frequency
+
+t = 0:0.001:1;
+
+% Carrier signal
+x = Amp * sin(2 * pi * fc * t);
+subplot(3, 1, 1);
+plot(t, x);
+grid on;
+xlabel('time');
+ylabel('amplitude');
+title('Apil Maraseni/Carrier Signal');
+legend('carrier wave');
+
+% Message signal (bipolar square wave using sign function)
+y = sign(sin(2 * pi * fm * t));
+subplot(3, 1, 2);
+plot(t, y);
+grid on;
+xlabel('time');
+ylabel('amplitude');
+title('Apil Maraseni/Message Signal');
+legend('message signal');
+
+% PSK Modulation (invert carrier phase based on message bit)
+a = x .* y;
+subplot(3, 1, 3);
+plot(t, a);
+grid on;
+xlabel('time');
+ylabel('amplitude');
+title('Apil Maraseni/PSK Signal');
+legend('PSK signal');
